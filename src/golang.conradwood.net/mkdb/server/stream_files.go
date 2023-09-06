d14fd6d920aed2aff79cb75091a8e8a1665eacc8
Conrad Wood <cnw@conradwood.net>
2023-09-06 06:57:10 +0100

    accept to write multiple files
    Change-Id: If74e662e02cd891d0d9ef3828148c5f9a080b37b

--- src/golang.conradwood.net/mkdb/server/stream_files.go
+++ src/golang.conradwood.net/mkdb/server/stream_files.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"fmt"
 	pb "golang.conradwood.net/apis/mkdb"
-	"golang.conradwood.net/mkdb/creator"
+	//	"golang.conradwood.net/mkdb/creator"
+	"strings"
 )
 
 func (e *echoServer) CreateDBFiles(req *pb.CreateDBRequest, srv pb.MKDB_CreateDBFilesServer) error {
-	return creator.CreateDBFiles(req, srv)
+	ctx := srv.Context()
+	res, err := e.CreateDBFile(ctx, req)
+	if err != nil {
+		return err
+	}
+	for _, s := range strings.Split(res.GoFile, "\n") {
+		fname := fmt.Sprintf("db-%s.go", req.Message)
+		s = s + "\n"
+		sd := &pb.FileStream{Filename: fname, Data: []byte(s)}
+		err = srv.Send(sd)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
